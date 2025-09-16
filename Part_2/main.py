a7400bbd859869267fc3e3e238cb37a912de083e
HugoSachim <hugosachim@ua.pt>
2025-09-16 12:01:49 +0100

    test_3

--- Part_2/main.py
+++ Part_2/main.py
@@ -1,7 +1,13 @@
 import cv2
 
 def main():
-    print("Hello World")
-    
-if __name__ == '__name__':
+    image_path = r"C:\Users\hogu\Desktop\SAVI-Hugo-Sachim_108510-25-26\Part_2\lake.jpg"
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+
+
+    cv2.imshow('Image', image)
+    cv2.waitKey(0)
+
+if __name__ == '__main__':
     main()
